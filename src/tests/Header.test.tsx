b9fed64538ad1895c853feb2684de3b038e14809
Sam Elsamman <sam.elsamman@gmail.com>
2021-07-26 16:02:35 -0400

    Updated test

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -4,17 +4,8 @@ import {StyleContext, StyleController} from "../controllers/StyleController";
 import {ListContext, ListController} from "../controllers/ListController";
 import {Header} from "../components/Header";
 import {render, screen} from "@testing-library/react";
-import {makeObservable} from "proxily";
-function makeMockFromClass<T>(c : abstract new (...args: any) => T, jestfn : Function, o: Partial<T>) : T {
-    const proto = c.prototype.constructor.prototype
-    Object.getOwnPropertyNames(proto).forEach(p => {
-        const props = Object.getOwnPropertyDescriptor(proto, p);
-        if (typeof props?.get !== "function"  && typeof proto[p] == 'function')
-            (o as unknown as any)[p] = jestfn();
+import {makeObservable, jestMockFromClass} from "proxily";
 
-    })
-    return o as T
-}
 describe('Header', () => {
     it('can add and delete', () => {
         //const mockListController = makeObservable(new ListController(new ToDoList()));
@@ -25,8 +16,7 @@ describe('Header', () => {
             addItem = jest.fn();
         }
         const mockListController = makeObservable(new MockListController(new ToDoList()));
-
-        function Mock () {
+              function Mock () {
             return (
                 <StyleContext.Provider value={new StyleController(new TodoListStyle())}>
                     <ListContext.Provider value={mockListController}>
@@ -53,8 +43,8 @@ describe('Header', () => {
                 </StyleContext.Provider>
             );
         }
-        const mockListController = makeMockFromClass(ListController, jest.fn,{});
-        const mockStyleController = makeMockFromClass(StyleController, jest.fn, {
+        const mockListController = jestMockFromClass(ListController, {});
+        const mockStyleController = jestMockFromClass(StyleController,  {
             get navbarButtonVariant () {return "secondary"}
         })
         render (<Mock mockListController={mockListController} mockStyleController={mockStyleController}/>);
